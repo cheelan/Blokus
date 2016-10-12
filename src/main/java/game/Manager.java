05813926e6587f67610bbaf3d7c85733765d5238
cheelan <cjh276@cornell.edu>
2016-10-11 22:11:22 -0700

    Manager recognizes gameover condition

--- src/main/java/game/Manager.java
+++ src/main/java/game/Manager.java
@@ -1,67 +1,55 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Manager {
 
-	public enum GameState {
-		PLAYER1_TURN,
-		PLAYER2_TURN,
-		GAME_OVER;
-	}
-	
-	private GameState state = GameState.PLAYER1_TURN;
-	private Player player1;
-	private Player player2;
 	private Board board;
+	private List<Player> players;
 	
-	public Manager(Player player1, Player player2, Board board) {
-		this.player1 = player1;
-		this.player2 = player2;
+	public Manager(List<Player> players, Board board) {
+		this.players = players;
 		this.board = board;
 	}
 	
-	public void go() {
-		Move nextMove = null;
-		switch (state) {
-			case PLAYER1_TURN:
-				nextMove = player1.play(board);
-				break;
-			case PLAYER2_TURN:
-				nextMove = player2.play(board);
-				break;
-			case GAME_OVER:
-				return;
-		}
-		try {
-			board.applyMove(nextMove);
-			// Remove nextMove from player
-			// TODO Check game over condition
-			if (state == GameState.PLAYER1_TURN) {
-				player1.removePiece(nextMove.getPiece().getName());
-				state = GameState.PLAYER2_TURN;
-			} else {
-				player2.removePiece(nextMove.getPiece().getName());
-				state = GameState.PLAYER1_TURN;
+	/**
+	 * Steps through one round of player turns. Returns true iff the game is over
+	 * @return true iff the game is over, false otherwise
+	 */
+	public boolean go() {
+		int movesPlayed = 0;
+		for (Player player : players) {
+			Move nextMove = player.play(board);
+			if (nextMove != null) {
+				board.applyMove(nextMove);
+				player.removePiece(nextMove.getPiece().getName());
+				movesPlayed++;
 			}
-		} catch (IllegalArgumentException e) {
-			// TODO Display some feedback for the player
-		}		
+		}
+		return movesPlayed == 0;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Start");
 		Player player1 = new Player(1, Piece.getAllPieces(1), new RandomController());
 		Player player2 = new Player(2, Piece.getAllPieces(2), new RandomController());
+		List<Player> players = new ArrayList<Player>();
+		players.add(player1);
+		players.add(player2);
 		Board board = new Board(new MoveValidator());
-		Manager manager = new Manager(player1, player2, board);
+		Manager manager = new Manager(players, board);
 		Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < 12; i++) {
+		boolean isGameOver = false;
+		while(!isGameOver) {
 			String s = scan.next();
-			manager.go();
+			isGameOver = manager.go();
 			System.out.println("Move " + board.getMoves());
 			System.out.println(board);
 		}
+		scan.close();
+		System.out.println("GAME OVER");
 		// TODO current rules are that you have to start at a corner, should be a specific corner
 	}
 	
